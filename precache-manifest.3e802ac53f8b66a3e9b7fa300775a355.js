18d1f1023398dd09553ffc01162658f1b1a49acb
reedtlr <69362002+reedtlr@users.noreply.github.com>
2020-10-28 20:01:47 -0400

    Updates

---
+++ precache-manifest.3e802ac53f8b66a3e9b7fa300775a355.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c52c4f3717a5acd4729a42476ee0ee28",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "4a2b6b60abf2f76d9139",
+    "url": "/react-portfolio/static/css/2.4d21caa1.chunk.css"
+  },
+  {
+    "revision": "99e6b894f77c3e44bc40",
+    "url": "/react-portfolio/static/css/main.c5765b89.chunk.css"
+  },
+  {
+    "revision": "4a2b6b60abf2f76d9139",
+    "url": "/react-portfolio/static/js/2.fa0ef9d9.chunk.js"
+  },
+  {
+    "revision": "99e6b894f77c3e44bc40",
+    "url": "/react-portfolio/static/js/main.13460d2e.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
