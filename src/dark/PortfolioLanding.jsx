a2750e8ad2d1393df3ebcd3a2ada0c9221d7e721
reedtlr <69362002+reedtlr@users.noreply.github.com>
2020-10-29 20:00:01 -0400

    fixed src img

--- src/dark/PortfolioLanding.jsx
+++ src/dark/PortfolioLanding.jsx
@@ -125,7 +125,7 @@ const PortfolioLanding = () => {
             {/* Start COntact Area */}
             <div id="contact" className="fix">
                 <div className="rn-contact-area ptb--120 bg_color--1">
-                    <ContactThree contactImages={require("./../public/assets/images/about/about-9.jpg")} contactTitle="Hire Me." />
+                    <ContactThree contactImages={require("./../../public/assets/images/about/about-9.jpg")} contactTitle="Hire Me." />
                 </div>
             </div>
             {/* End COntact Area */}
